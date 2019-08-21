34e799aef42a5f3c6f5da8a2c6c1c4393669aa82
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-21 15:33:58 +0000

    Snapshot of branch master taken on 2019-08-21T11:33:57.73-04:00 from "https://github.com/UnofficialJuliaMirror/ShapesOfVariables.jl-b3480ff6-a2af-506e-a6b8-61d160ca6532"

--- src/varshapes.jl
+++ src/varshapes.jl
@@ -13,7 +13,24 @@ end
 VariableDataAccessor(shape::S, offset::Int) where {S<:AbstractValueShape} = VariableDataAccessor{S}(shape, offset)
 
 
+const ScalarAccessor{T} = VariableDataAccessor{ScalarShape{T}} where {T}
+const ArrayAccessor{T,N} = VariableDataAccessor{ArrayShape{T,N}} where {T,N}
+const ConstAccessor = VariableDataAccessor{ConstValueShape{T}} where {T}
+
+
 nonabstract_eltype(va::VariableDataAccessor) = nonabstract_eltype(va.shape)
+
+AbstractValueShape(va::VariableDataAccessor) = va.shape
+Base.convert(::Type{AbstractValueShape}, va::VariableDataAccessor) = ValueShape(va)
+
+Base.size(va::VariableDataAccessor) = size(va.shape)
+Base.length(va::VariableDataAccessor) = length(va.shape)
+
+
+# ToDo: implement Base.getindex for VariableDataAccessor? Allow only contiguous?
+
+
+
 
 
 @inline function _view_range(idxs::AbstractUnitRange{<:Integer}, va::VariableDataAccessor)
@@ -22,46 +39,63 @@ nonabstract_eltype(va::VariableDataAccessor) = nonabstract_eltype(va.shape)
     from:to
 end
 
-
-const ScalarAccessor{T} = VariableDataAccessor{ScalarShape{T}} where {T}
-const ArrayAccessor{T,N} = VariableDataAccessor{ArrayShape{T,N}} where {T,N}
-const ConstAccessor = VariableDataAccessor{ConstValueShape{T}} where {T}
+@inline _view_idxs(idxs::AbstractUnitRange{<:Integer}, va::VariableDataAccessor) = _view_range(idxs, va)
+@inline _view_idxs(idxs::AbstractUnitRange{<:Integer}, va::ScalarAccessor) = first(idxs) + va.offset
 
 
 Base.@propagate_inbounds (va::ScalarAccessor)(data::AbstractVector) =
-    data[first(_view_range(axes(data, 1), va))]
+    data[_view_idxs(axes(data, 1), va)]
 
 Base.@propagate_inbounds (va::ArrayAccessor{T,1})(data::AbstractVector) where {T} =
-    view(data, _view_range(axes(data, 1), va))
+    view(data, _view_idxs(axes(data, 1), va))
 
 Base.@propagate_inbounds (va::ArrayAccessor{T,N})(data::AbstractVector) where {T,N} =
-    reshape(view(data, _view_range(axes(data, 1), va)), size(va.shape)...)
+    reshape(view(data, _view_idxs(axes(data, 1), va)), size(va.shape)...)
 
 @inline (va::ConstAccessor)(::AbstractVector) = va.shape.value
 
 
 Base.@propagate_inbounds function (va::ScalarAccessor)(data::AbstractVectorOfSimilarVectors)
     flat_data = flatview(data)
-    idxs = _view_range(axes(flat_data, 1), va)
-    view(flat_data, first(idxs), :)
+    idxs = _view_idxs(axes(flat_data, 1), va)
+    view(flat_data, idxs, :)
 end
 
 Base.@propagate_inbounds function (va::ArrayAccessor{T,1})(data::AbstractVectorOfSimilarVectors) where {T,N}
     flat_data = flatview(data)
-    idxs = _view_range(axes(flat_data, 1), va)
+    idxs = _view_idxs(axes(flat_data, 1), va)
     fpview = view(flat_data, idxs, :)
     VectorOfSimilarVectors(fpview)
 end
 
 Base.@propagate_inbounds function (va::ArrayAccessor{T,N})(data::AbstractVectorOfSimilarVectors) where {T,N}
     flat_data = flatview(data)
-    idxs = _view_range(axes(flat_data, 1), va)
+    idxs = _view_idxs(axes(flat_data, 1), va)
     fpview = view(flat_data, idxs, :)
     VectorOfSimilarArrays(reshape(fpview, size(va.shape)..., :))
 end
 
 @inline (va::ConstAccessor)(data::AbstractVectorOfSimilarVectors) =
     Fill(va.shape.value, size(data,1))
+
+
+Base.@propagate_inbounds Base.getindex(data::AbstractVector, va::VariableDataAccessor) = va(data)
+
+
+Base.@propagate_inbounds Base.view(data::AbstractVector, va::VariableDataAccessor) = va(data)
+Base.@propagate_inbounds Base.view(data::AbstractVector{<:AbstractVector}, va::VariableDataAccessor) = va(data)
+
+
+Base.@propagate_inbounds function Base.view(data::AbstractVector, va::ScalarAccessor)
+    view(data, _view_idxs(axes(data, 1), va))
+end
+
+
+Base.@propagate_inbounds function Base.view(
+    data::AbstractMatrix, va_row::VariableDataAccessor, va_col::VariableDataAccessor
+)
+    view(data, _view_idxs(axes(data, 1), va_row), _view_idxs(axes(data, 2), va_col))
+end
 
 
 @inline _varoffset_cumsum_impl(s, x, y, rest...) = (s, _varoffset_cumsum_impl(s+x, y, rest...)...)
@@ -140,11 +174,11 @@ fill!(table.a, 4.2)
 all(x -> x == 4.2, view(flatview(data), 1, :))
 ```
 """
-struct VarShapes{N,AC}
-    _accessors::AC
+struct VarShapes{names,AT<:(NTuple{N,VariableDataAccessor} where N)}
+    _accessors::NamedTuple{names,AT}
     _flatdof::Int
 
-    @inline function VarShapes(varshapes::NamedTuple{PN,<:NTuple{N,AbstractValueShape}}) where {PN,N}
+    @inline function VarShapes(varshapes::NamedTuple{names,<:NTuple{N,AbstractValueShape}}) where {names,N}
         labels = keys(varshapes)
         shapes = values(varshapes)
         shapelengths = map(totalndof, shapes)
@@ -154,13 +188,13 @@ struct VarShapes{N,AC}
         # @assert shapelengths == acclengths
         n_flattened = sum(shapelengths)
         named_accessors = NamedTuple{labels}(accessors)
-        new{PN,typeof(named_accessors)}(named_accessors, n_flattened)
+        new{names,typeof(accessors)}(named_accessors, n_flattened)
     end
 end
 
 export VarShapes
 
-@inline VarShapes(;varshapes...) = VarShapes(varshapes.data)
+@inline VarShapes(;varshapes...) = VarShapes(values(varshapes))
 
 
 @inline _accessors(x::VarShapes) = getfield(x, :_accessors)
